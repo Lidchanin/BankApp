59c07dc05fec4e215e550f9b70ceac8865d38264
DESKTOP-AHL2VQ6\Lidchanin <zanuda.lidchanin@gmail.com>
2018-12-04 11:06:46 +0300

    Add project files.

---
+++ BankLibrary/AccountEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        public string Message { get; private set; }
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs (string _mess, decimal _sum)
+        {
+            Message = _mess;
+            Sum = _sum;
+        }
+    }
+}
